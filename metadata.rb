dca9f9eaddfb029bd9d356db103bbbb544b5b0b0
Ringo De Smet <ringo@releasequeue.com>
2014-04-18 14:43:10 +0200

    Adding NodeJS & NPM to the build tools setup.

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'ringo@automate-dev.com'
 license          'Apache 2.0'
 description      'Installs/Configures Automate.Dev Development Build Tools'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.2.0'
+version          '0.2.1'
 
 %w[ubuntu].each do |os|
   supports os
@@ -14,3 +14,4 @@ depends 'apt', '= 2.2.0'
 depends 'rvm', '= 0.9.2'
 depends 'java', '= 1.22.0'
 depends 'maven', '= 1.1.0'
+depends 'nodejs', '= 1.3.0'
