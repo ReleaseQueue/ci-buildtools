d49e7cc74775496e99fd69be3dbdd496b534857e
Ringo De Smet <ringo@releasequeue.com>
2014-08-10 16:46:02 +0200

    Release 0.3.0

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'ringo@automate-dev.com'
 license          'Apache 2.0'
 description      'Installs/Configures Automate.Dev Development Build Tools'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.2.1'
+version          '0.3.0'
 
 %w[ubuntu].each do |os|
   supports os
