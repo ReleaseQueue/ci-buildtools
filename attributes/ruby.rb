30cc771196ad365b222243a8a1a1496d2ad93957
Ringo De Smet <ringo@releasequeue.com>
2014-11-14 15:07:08 +0100

    RELEASEQ-84: Upgrading everything to Ruby 2.1.5. This version has binaries available for Ubuntu 12.04, 14.04 and Mac OS X 10.9 and 10.10

--- attributes/ruby.rb
+++ attributes/ruby.rb
@@ -1,5 +1,8 @@
 # If you update the ruby versions, please update the integration tests under
 #   test/integration/default/bats/ruby.bats
-override['rvm']['default_ruby'] = 'ruby-2.1.4'
-override['rvm']['rubies'] = %w(ruby-2.1.4)
+override['rvm']['default_ruby'] = 'ruby-2.1.5'
+override['rvm']['rubies'] = %w(ruby-2.1.3 ruby-2.1.5)
 override['rvm']['group_users'] = node['ad-buildtools']['developers']
+override['rvm']['global_gems'] = [
+    { 'name'    => 'pkgr' }
+]
