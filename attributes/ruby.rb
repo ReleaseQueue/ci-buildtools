b5da57fa62223bf1588d5f4c1ef9e8ca5470d3f2
Ringo De Smet <ringo@releasequeue.com>
2014-11-14 15:20:00 +0100

    RELEASEQ-84: Fixing Rubocop violations.

--- attributes/ruby.rb
+++ attributes/ruby.rb
@@ -3,6 +3,7 @@
 override['rvm']['default_ruby'] = 'ruby-2.1.5'
 override['rvm']['rubies'] = %w(ruby-2.1.3 ruby-2.1.5)
 override['rvm']['group_users'] = node['ad-buildtools']['developers']
-override['rvm']['global_gems'] = [
-    { 'name'    => 'pkgr' }
-]
+override['rvm']['global_gems'] =
+  {
+    'name' => 'pkgr'
+  }
