c17fdf40ed848846bd08f94ea6eaa7f88685d7c7
Ringo De Smet <ringo@releasequeue.com>
2014-11-14 15:24:28 +0100

    RELEASEQ-84: Chef attribute requires a collection value.

--- attributes/ruby.rb
+++ attributes/ruby.rb
@@ -3,7 +3,8 @@
 override['rvm']['default_ruby'] = 'ruby-2.1.5'
 override['rvm']['rubies'] = %w(ruby-2.1.3 ruby-2.1.5)
 override['rvm']['group_users'] = node['ad-buildtools']['developers']
-override['rvm']['global_gems'] =
+override['rvm']['global_gems'] = [
   {
     'name' => 'pkgr'
   }
+]
