7d4ab71389a23a14e8882c37e22945e23eb31305
Ringo De Smet <ringo@releasequeue.com>
2014-11-16 12:45:16 +0100

    RELEASEQ-88: Adding deb-s3 gem to ruby setup.

--- attributes/ruby.rb
+++ attributes/ruby.rb
@@ -4,7 +4,6 @@ override['rvm']['default_ruby'] = 'ruby-2.1.5'
 override['rvm']['rubies'] = %w(ruby-2.1.3 ruby-2.1.5)
 override['rvm']['group_users'] = node['ad-buildtools']['developers']
 override['rvm']['global_gems'] = [
-  {
-    'name' => 'pkgr'
-  }
+  { 'name' => 'pkgr' },
+  { 'name' => 'deb-s3' }
 ]
