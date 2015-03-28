d0193effc1bc8d9a0e07fbc09c4fea7c9d379fd3
Ringo De Smet <ringo@releasequeue.com>
2015-03-28 17:57:56 +0100

    Release 0.3.10

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'ringo@releasequeue.com'
 license          'Apache 2.0'
 description      'Installs/Configures ReleaseQueue Development Build Tools'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.3.9'
+version          '0.3.10'
 
 supports 'ubuntu', '= 14.04'
 supports 'mac_os_x', '= 10.9'
