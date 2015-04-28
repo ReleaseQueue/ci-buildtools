44e51a9ffcb2119d71ef7e3757e44be1e9c268e7
Ringo De Smet <ringo@releasequeue.com>
2015-04-28 22:36:44 +0200

    Upgrading from Java 7 to Java 8

--- attributes/java.rb
+++ attributes/java.rb
@@ -1,4 +1,4 @@
-# Prefer Oracle Java 7
-override['java']['jdk_version'] = '7'
+# Prefer Oracle Java 8
+override['java']['jdk_version'] = '8'
 override['java']['install_flavor'] = 'oracle'
 override['java']['oracle']['accept_oracle_download_terms'] = true
